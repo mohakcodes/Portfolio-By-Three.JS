a3688b210515dcd5cbc174baa433bae332083b79
Mohak Chauhan <mohak46812@gmail.com>
2023-04-01 13:19:19 +0530

    added navbar and home section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,55 @@
 import React from 'react'
 
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { ComputersCanvas } from './canvas'
+
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
-  )
+    <section className='relative w-full h-screen mx-auto pb-0'>
+
+      <div className={`${styles.paddingX} absolute inset-0 top-[110px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#913eff]'/>
+          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText}`}>
+            Hi, I am <span className='text-[#915eff]'> Mohak </span>
+          </h1>
+          <p className={`${styles.heroSubText} text-white-100`}>
+            I Love to Develop Web Applications, <br/>
+            Frontend Interfaces & <br/>
+            Practice Problem Solving
+          </p>
+        </div>
+
+      </div>
+
+      <ComputersCanvas/>
+
+      <div className='absolute xs:bottom-2 bottom-10 w-full flex justify-center items-center'>
+        <a href="#about" className=''>
+          <div className='w-[25px] h-[45px] rounded-3xl border-2 border-white flex justify-center items-start'>
+            <motion.dev
+              animate={{
+                y: [0, 25, 0]
+              }}
+              transition={{
+                duration:1.2,
+                repeat:Infinity,
+                repeatType:'loop', 
+              }}
+              className="w-3 h-3 rounded-full bg-white mb-1" 
+            />
+          </div>
+        </a>
+      </div>
+
+    </section>
+  ) 
 }
 
 export default Hero
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 
-const Computers = () => {
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload , useGLTF } from '@react-three/drei'
+import Canvasloader from '../Loader'
+
+const ComputersCanvas = () => {
+
+  const [isMobile , setIsMobile] = useState(false);
+
+  useEffect(()=>{
+    const mQuery = window.matchMedia('(max-width:500px)');
+    setIsMobile(mQuery.matches);
+    console.log(mQuery.matches);
+
+    const handleMQuerychange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    mQuery.addEventListener("change" , handleMQuerychange);
+
+    return() => {
+      mQuery.removeEventListener("change" , handleMQuerychange);
+    }
+
+  } , []);
+
+  const Computers = ({isMobile}) => {
+    const computer = useGLTF("./desktop_pc/scene.gltf")
+      return (
+      <mesh>
+        <hemisphereLight intensity={0.15} groundColor="black"/>
+        <pointLight intensity={1}/>
+        <spotLight
+          position={[-20, 50, 10]}
+          angle={0.12}
+          penumbra={1}
+          intensity={1}
+          castShadow
+          shadow-mapSize={1024}
+        />
+        <primitive
+          object={computer.scene}
+          scale={isMobile ? 0.55 : 0.7}
+          position={isMobile ? [0 , -3.3 , -1.2] : [0 , -3.5 , -1.3]}
+          rotation={[0 , 0 , -0.03]}
+        />
+      </mesh>
+    )
+  }
+
   return (
-    <div>Computers</div>
-  )
-}
+    <Canvas frameloop='demand'
+            shadows
+            camera={{position:[20,3,5] , fov:25}}
+            gl={{preserveDrawingBuffer:true}}
+            className={isMobile ? 'pt-14' : 'pt-10'}
+    >
 
-export default Computers
+      <Suspense fallback={<Canvasloader/>}>
+        <OrbitControls
+          enableZoom = {false}
+          maxPolarAngle = {Math.PI / 2}
+          minPolarAngle = {Math.PI / 2}
+        />
+        <Computers isMobile = {isMobile}/>
+      </Suspense>
+
+      <Preload all />
+      
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas; 
